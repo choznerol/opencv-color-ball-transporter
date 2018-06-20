80c164eb1d9e28a6ed03e1d7fc210beedddfc76b
choznerol <lawrencechou1024@gmail.com>
2018-06-21 01:08:15 +0800

    Update color hue

--- config/colors.py
+++ config/colors.py
@@ -5,9 +5,13 @@ BALL_COLOR_HSV_RANGE = {
     "BLUE": [np.array([90, 50, 50]), np.array([120, 255, 255])],
     # Green's hue is 60
     "GREEN": [np.array([45, 50, 50]), np.array([70, 255, 255])],
-    "RED": [np.array([0, 50, 50]), np.array([10, 255, 255])],  # FIXME: 會被覆蓋
     # Red's hue is 0
-    "RED": [np.array([170, 50, 50]), np.array([180, 255, 255])]
+    # "RED": [np.array([0, 50, 50]), np.array([10, 255, 255])],  # FIXME: 會被覆蓋
+    "RED": [np.array([170, 50, 50]), np.array([190, 255, 255])],
+    # Yellow's hue is 0
+    "YELLOW": [np.array([80, 50, 50]), np.array([90, 255, 255])],
+    # Purple's hue is 0
+    "PURPLE": [np.array([150, 50, 50]), np.array([165, 255, 255])]
 }
 
 DEST_COLOR_HSV_RANGE = {
@@ -15,7 +19,11 @@ DEST_COLOR_HSV_RANGE = {
     "BLUE": [np.array([90, 50, 50]), np.array([120, 255, 255])],
     # Green's hue is 60
     "GREEN": [np.array([45, 50, 50]), np.array([70, 255, 255])],
-    "RED": [np.array([0, 50, 50]), np.array([10, 255, 255])],  # FIXME: 會被覆蓋
     # Red's hue is 0
-    "RED": [np.array([170, 50, 50]), np.array([180, 255, 255])]
+    # "RED": [np.array([0, 50, 50]), np.array([10, 255, 255])],  # FIXME: 會被覆蓋
+    "RED": [np.array([170, 50, 50]), np.array([190, 255, 255])],
+    # Yellow's hue is 0
+    "YELLOW": [np.array([80, 50, 50]), np.array([90, 255, 255])],
+    # Purple's hue is 0
+    "PURPLE": [np.array([150, 50, 50]), np.array([165, 255, 255])]
 }
